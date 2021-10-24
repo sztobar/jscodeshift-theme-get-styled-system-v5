ed3cc158e33c5d6ae917e81fda0d1df3952135fd
sztobar <bszewczyk91@gmail.com>
2021-10-24 21:35:31 +0200

    Config bin files
    * properly configure bin files
    * added `outDir` and `declaration: true` in tsconfig.json
    * added install script in package.json

--- bin/jscodeshift-theme-get-styled-system-v5.js
+++ bin/jscodeshift-theme-get-styled-system-v5.js
@@ -1,6 +1,6 @@
+#!/usr/bin/env node
 const path = require('path');
 
-const cwd = process.cwd();
-const transformPath = path.resolve(cwd, '../src/theme-get.ts');
+const transformPath = require.resolve('../dist/theme-get.js');
 process.argv.push(`-t=${transformPath}`);
-import('jscodeshift/bin/jscodeshift');
+import('jscodeshift/bin/jscodeshift.js');
---
+++ bin/jscodeshift-theme-get-styled-system-v5.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+'use strict';
+
+require('./jscodeshift-theme-get-styled-system-v5.js');
